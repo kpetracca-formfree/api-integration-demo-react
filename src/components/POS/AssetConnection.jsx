714f9c3f13bf03642c04b5f6167dab5c4a72a6f2
Karley Petracca <kpetracca@formfree.com>
2020-06-17 18:27:13 -0400

    Start implementation of POS downstream components

---
+++ src/components/POS/AssetConnection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AssetConnection = (props) => {
+  return (
+    <div>
+      <p>assets</p>
+    </div>
+  );
+};
+
+export default AssetConnection;
---
+++ src/components/POS/VOAReport.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const VOAReport = (props) => {
+  return (
+    <div>
+      <p>report</p>
+    </div>
+  );
+};
+
+export default VOAReport;
