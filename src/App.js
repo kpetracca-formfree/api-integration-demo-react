87f704e63b9f19ed44c6acc200b0fa6a87b9129c
Karley Petracca <kpetracca@formfree.com>
2020-06-17 18:25:23 -0400

    Built out routing for LOS and POS demo branches

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 
+// top level imports
 import DemoControl from "./components/DemoControl";
 import DemoNav from "./components/DemoNav";
-import DemoWindow from "./components/DemoWindow";
+import DemoWindow from "./components/DemoWindow"; // routing import
+
+// POS routing imports
+import StartApplication from "./components/POS/StartApplication";
+import AssetConnection from "./components/POS/AssetConnection";
+import VOAReport from "./components/POS/VOAReport";
+
+// NOTE: LOS is more like SPA format versus different routing in POS
 
 import "./App.css";
 import styled from "styled-components";
@@ -17,6 +25,10 @@ const AppStyled = styled.div`
 
   .demoWrapper {
     width: 100%;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
   }
 `;
 
@@ -24,13 +36,19 @@ const App = () => {
   return (
     <RecoilRoot>
       <AppStyled>
-        <DemoControl />
-        <div className="demoWrapper">
-          <DemoNav />
-          <Router>
+        <Router>
+          <DemoControl />
+          <div className="demoWrapper">
+            <DemoNav />
             <Route path="/" exact component={DemoWindow} />
-          </Router>
-        </div>
+            <Route path="/application/start" component={StartApplication} />
+            <Route
+              path="/application/assets/:orderId"
+              component={AssetConnection}
+            />
+            <Route path="/application/report/:orderId" component={VOAReport} />
+          </div>
+        </Router>
       </AppStyled>
     </RecoilRoot>
   );
