0ff5e05c280fe632450d0e640dc56edafd89eff2
Karley Petracca <kpetracca@formfree.com>
2020-06-22 18:12:07 -0400

    Add embedded pdf with page controls

--- src/components/POS/ApplicationComplete.jsx
+++ src/components/POS/ApplicationComplete.jsx
@@ -1,6 +1,19 @@
 import React, { useState, useEffect } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/Page/AnnotationLayer.css";
 
 import { GET, GET_PDF } from "../../utils/apiCalls";
+
+import styled from "styled-components";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+const ApplicationCompleteStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+`;
 
 const ApplicationComplete = (props) => {
   const { orderId } = props.match.params;
@@ -8,6 +21,9 @@ const ApplicationComplete = (props) => {
   const [latestReportId, setLatestReportId] = useState("");
   const [latestReportJson, setLatestReportJson] = useState("");
   const [latestReportPdf, setLatestReportPdf] = useState("");
+
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
 
   useEffect(() => {
     const getReportInfo = async (orderId) => {
@@ -40,12 +56,50 @@ const ApplicationComplete = (props) => {
     getReportPdf();
   };
 
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+  };
+
+  const previousPage = () => {
+    setPageNumber(pageNumber - 1);
+  };
+
+  const nextPage = () => {
+    setPageNumber(pageNumber + 1);
+  };
+
   return (
-    <div>
+    <ApplicationCompleteStyled>
       <p>Completion Dashboard</p>
       <button onClick={getReports}>Get Report</button>
-      <iframe src={latestReportPdf} type="application/pdf" />
-    </div>
+      <Document
+        file={latestReportPdf}
+        onLoadError={(error) =>
+          alert("Error while loading document! " + error.message)
+        }
+        onLoadSuccess={onDocumentLoadSuccess}
+        options={{ cMapUrl: "cmaps/", cMapPacked: true }}
+      >
+        <Page pageNumber={pageNumber} />
+      </Document>
+      {numPages ? (
+        <p>
+          Page {pageNumber} of {numPages}
+        </p>
+      ) : (
+        ""
+      )}
+      {pageNumber > 1 ? (
+        <button onClick={previousPage}>Previous page</button>
+      ) : (
+        ""
+      )}
+      {pageNumber < numPages ? (
+        <button onClick={nextPage}>Next page</button>
+      ) : (
+        ""
+      )}
+    </ApplicationCompleteStyled>
   );
 };
 
